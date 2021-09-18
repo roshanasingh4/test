2d4235bc590405ac006e746c7dcf8eae479812eb
sukhendu2002 <sukhenduguria2017@gmail.com>
2021-09-18 17:32:48 +0530

    add fetch functionality

--- routes/fetchData.js
+++ routes/fetchData.js
@@ -1,7 +1,17 @@
-const express = require('express');
-const { db } = require('../model/pushDataModel');
-const Notes = require('../model/pushDataModel');
+const express = require("express");
+const { db } = require("../model/pushDataModel");
+const Notes = require("../model/pushDataModel");
 const router = express.Router();
 
+//fetches data by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Notes.find({ id: id }, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(data);
+  });
+});
 
 module.exports = router;
